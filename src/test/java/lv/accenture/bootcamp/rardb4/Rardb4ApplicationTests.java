2ed862ef8dd1843464a460e8cce9926e56e83909
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-03-13 01:28:05 +0200

    Initial commit

---
+++ src/test/java/lv/accenture/bootcamp/rardb4/Rardb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.accenture.bootcamp.rardb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rardb4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
