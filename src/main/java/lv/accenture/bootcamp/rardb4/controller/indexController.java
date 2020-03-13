96094c310c5f5f16aa0a7bdd4e28de847934f5cc
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-03-13 23:16:07 +0200

    Controllers linked to repositories

--- src/main/java/lv/accenture/bootcamp/rardb4/controller/indexController.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/controller/indexController.java
@@ -1,6 +1,13 @@
 package lv.accenture.bootcamp.rardb4.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexController {
+
+    @GetMapping("/") //link to index page
+    public String toIndex() {
+        return "index";
+    }
 }
