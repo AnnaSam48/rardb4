06a8e2a8ee7b1bb3152b5b895b403b0d6141e938
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-03-14 11:17:46 +0200

    edited ReviewController and ReviewRepository

--- src/main/java/lv/accenture/bootcamp/rardb4/controller/ReviewController.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/controller/ReviewController.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ReviewController {
@@ -19,11 +24,18 @@ public class ReviewController {
 
         Iterable<Review> reviews = reviewRepository.findAll();
         model.addAttribute("reviews", reviews);
-
         return "reviews-index";
     }
 
-    @GetMapping("/review/add")
+    @GetMapping("/reviews/highest-rated-reviews")
+    public String highestReviews(@RequestParam double reviewRating, Model model) {
+        List<Review> highestRatedReviews = reviewRepository.findByReviewRating(reviewRating);
+        model.addAttribute("reviews", highestRatedReviews);
+        return "reviews-index";
+    }
+
+
+    @PostMapping("/review/add")
     public String addAReview(Model model) {
         model.addAttribute("review", new Review());
         return "add-review";
--- src/main/java/lv/accenture/bootcamp/rardb4/repository/ReviewRepository.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/repository/ReviewRepository.java
@@ -1,10 +1,15 @@
 package lv.accenture.bootcamp.rardb4.repository;
 
 import lv.accenture.bootcamp.rardb4.model.Review;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends CrudRepository<Review, Long> {
-
+    @Query("SELECT review FROM Review review WHERE review.reviewRating (highest)")
+    List<Review> findByReviewRating(@Param(value = "reviewRating") double reviewRating);
 }
