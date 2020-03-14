2dd305a6fae683c2418d9702c560d9e3e273898a
LieneA <58346249+LieneA@users.noreply.github.com>
2020-03-14 10:22:13 +0200

    Update Movie.java

--- src/main/java/lv/accenture/bootcamp/rardb4/model/Movie.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/model/Movie.java
@@ -5,4 +5,21 @@ import java.io.Serializable;
 
 @Entity
 public class Movie implements Serializable {
+  
+  @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long movieID;
+	private String movieName;
+	private int Year;
+	
+	
+	public Movie(Long movieID, String movieName, int year) {
+		this.movieID = movieID;
+		this.movieName = movieName;
+		Year = year;
+	}
+
+
+	public Movie() {
+	}
 }
