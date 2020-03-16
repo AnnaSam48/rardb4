7d63037d941cfe02d6f05b459844bce527af0b87
pbm222 <innapbm@gmail.com>
2020-03-16 19:10:20 +0200

    movies found

--- src/main/java/lv/accenture/bootcamp/rardb4/controller/MovieController.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/controller/MovieController.java
@@ -2,6 +2,7 @@ package lv.accenture.bootcamp.rardb4.controller;
 
 import lv.accenture.bootcamp.rardb4.MovieAPI.MovieAPIService;
 import lv.accenture.bootcamp.rardb4.model.Movie;
+import lv.accenture.bootcamp.rardb4.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +20,10 @@ import java.util.Optional;
 public class MovieController {
 
     @Autowired
-    MovieAPIService movieAPIService;
+    private MovieAPIService movieAPIService;
+
+    @Autowired
+    private MovieRepository moviesRepository;
 
     @GetMapping("/add-review-search")
     public String addReviewSearchPage() {
@@ -29,18 +33,29 @@ public class MovieController {
 
     @GetMapping("/add-review-search/search")
     public String searchMoviesByKeyword(Model model, @RequestParam String keyword) {
-        List<Movie> foundMovies = movieAPIService.getMovie( keyword);
+        List<Movie> foundMovies = movieAPIService.getMovie(keyword);
         model.addAttribute("movies", foundMovies);
         return "add-review-search"; //if we do redirect then we loose parameter
     }
 
-    @GetMapping("/add-review-search/add-review-movie")
-    public String addReviewToTheMovie(){
+    /*@PostMapping("/add-review-search/{id}") //where we are getting data from
+    public String saveChosenMovie(@PathVariable String id, Movie movieEditted, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            return "add-review-search";
+        }else {
+            movieEditted.setImdbID(id);
+            moviesRepository.save(movieEditted);
+            //System.out.println("Movie was added");
+            return "add-review-search/add-review-movie";
+        }
+    }*/
 
+    @GetMapping("/add-review-search/add-review-movie/{id}")
+    public String viewAndAddReviewPage(@PathVariable String id, Model model) { //this id is the same id in URL
+        Movie movieToShow = movieAPIService.getMovieByID(id);
+        model.addAttribute("movie", movieToShow); //with what data we are working with
         return "add-review-movie";
     }
-
-
 
 
 }
