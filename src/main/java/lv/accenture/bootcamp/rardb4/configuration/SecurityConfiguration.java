0102f8935ef10647ece20998329f01ab3163af12
krista-tech <kristabokanova@gmail.com>
2020-04-03 20:36:36 +0300

    security check

--- src/main/java/lv/accenture/bootcamp/rardb4/configuration/SecurityConfiguration.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/configuration/SecurityConfiguration.java
@@ -14,6 +14,4 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 
     @Bean
     public DelegatingPasswordEncoder passwordEncoder(){return (DelegatingPasswordEncoder) passwordEncoders;}
-
-
 }
