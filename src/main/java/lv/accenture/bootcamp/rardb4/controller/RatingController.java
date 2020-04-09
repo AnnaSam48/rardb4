7e8881f4e2839a5ca3c88e9621fb34ef5d1fd669
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-04-09 14:32:18 +0300

    Revert back. (Possible code to show user they have rated review at the commented out till solution with registered and not registered users will be found)

--- src/main/java/lv/accenture/bootcamp/rardb4/controller/RatingController.java
+++ src/main/java/lv/accenture/bootcamp/rardb4/controller/RatingController.java
@@ -26,20 +26,109 @@ import java.util.*;
 public class RatingController {
 
 
+
     @Autowired
     private CommentRepository commentRepository;
+
 
 
     @Autowired
     private ReviewRepository reviewRepository;
 
 
+
     @Autowired
     private RatingRepository ratingRepository;
 
-    @Autowired
-    private UserService userService;
+    @GetMapping("/reviews/reviews-search/rate-review/{id}")
+    public String editRatingPage(@PathVariable Long id, Model model) {
+        //Getting data from rating page
+        Optional<Review> reviewToBeRated = reviewRepository.findById(id);
+        List<Comment> allComments = commentRepository.findAllByReviewID(id);
 
+        model.addAttribute("comments", allComments);
+        model.addAttribute("review", reviewToBeRated.get());
+        model.addAttribute("commentOb", new Comment());
+        model.addAttribute("rating", new Rating());
+
+        return "rate-review";
+    }
+
+
+    @PostMapping("/reviews/reviews-search/rate-review/{id}") //all the data from here
+    public String saveRatings(@PathVariable Long id, @Valid Rating ratingToEdit, BindingResult bindingResult) {
+
+        //Getting the voter's userID
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserWithId userWithId = (UserWithId) auth.getPrincipal();
+        Long userRatingReview = userWithId.getUserId();
+
+
+        //Getting new Rating object
+        Rating newRating = new Rating();
+        int newRatingValue;
+
+        //Getting list of all the ratings for review
+        List<Rating> matchedRatings = ratingRepository.findAllByReviewId(id);
+
+        //Making a list of all users that have rated this review already
+        List<Long> foundUserIDS = new ArrayList<>();
+        for (Rating matchedRating : matchedRatings) {
+            foundUserIDS.add(matchedRating.getRatedByUserId());
+        }
+
+        //checking if voter is also the author of review
+        try {
+            if (reviewRepository.findByReviewID(id).get().getUserId() == (userRatingReview)) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            return "rating-submit-error";
+        }
+
+        //checking if voter has already rated review before
+        try {
+            for (Long foundUserID : foundUserIDS) {
+                if (foundUserID == (userRatingReview)) {
+                    throw new IllegalArgumentException();
+                }
+            }
+
+        } catch (IllegalArgumentException ee) {
+            return "rating-submit-error";
+        }
+        if (bindingResult.hasErrors()) {
+            return "rating-submit-error";
+
+        } else {
+
+            //getting the new rating value
+            newRatingValue = ratingToEdit.getValue();
+            //setting the new rating value in review and rating classes
+
+            newRating.setReviewId(id);
+            newRating.setValue(newRatingValue);
+            newRating.setRatedByUserId(userRatingReview);
+            ratingRepository.save(newRating);
+
+
+            Review reviewToAddRating = reviewRepository.findByReviewID(id).get();
+            reviewToAddRating.setReviewID(id);
+            reviewToAddRating.setReviewRating(ratingRepository.average(id));
+            reviewRepository.save(reviewToAddRating);
+
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/reviews/rating-submitting-error")
+    public String submitError() {
+        return "rating-submit-error";
+    }
+}
+
+/*
+trying to show if the reviiew has already been rated under review text
     @GetMapping("/reviews/reviews-search/rate-review/{id}")
     public String editRatingPage(@PathVariable Long id, Model model) {
 
@@ -50,8 +139,10 @@ public class RatingController {
 
         //user data
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if(auth.getPrincipal().equals("USER")) {
+        if(!auth.isAuthenticated()) {
+            reviewToBeRated.get().setAlreadyRatedThisReview("");
 
+        }else{
             UserWithId userWithId = (UserWithId) auth.getPrincipal();
             Long userRatingReview = userWithId.getUserId();
 
@@ -66,8 +157,6 @@ public class RatingController {
                     reviewToBeRated.get().setAlreadyRatedThisReview("You have already rated this review.");
                 }
             }
-        }else{
-            reviewToBeRated.get().setAlreadyRatedThisReview("");
         }
 
 
@@ -133,9 +222,6 @@ public class RatingController {
             return "redirect:/";
         }
     }
+*/
 
-    @GetMapping("/reviews/rating-submitting-error")
-    public String submitError() {
-        return "rating-submit-error";
-    }
-}
+
